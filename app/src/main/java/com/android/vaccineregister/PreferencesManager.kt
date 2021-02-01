bee2fecd7459c665b20ce1734ba0a935c290d35a
Varun Mehta <varun.mehta@hey.com>
2021-02-01 20:10:31 +0530

    Add PreferencesManager.kt for managing IntroSlider. Modified WelcomeActivity.kt to use Shared Preferences for displaying IntroSlider
    Signed-off-by: Varun Mehta <varun.mehta@hey.com>

---
+++ app/src/main/java/com/android/vaccineregister/PreferencesManager.kt
@@ -0,0 +1,30 @@
+package com.android.vaccineregister
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferencesManager @SuppressLint("CommitPrefEdits")
+constructor(context: Context) {
+
+    private val preferences: SharedPreferences
+    private val editor: SharedPreferences.Editor
+
+    init {
+        preferences = context.getSharedPreferences(PREFERENCE_CONFIGURATION_NAME, PRIVATE_MODE)
+        editor = preferences.edit()
+    }
+
+    fun isFirstRun() = preferences.getBoolean(FIRST_TIME, true)
+
+    fun setFirstRun() {
+        editor.putBoolean(FIRST_TIME, false).commit()
+        editor.commit()
+    }
+
+    companion object {
+        private const val PRIVATE_MODE = 0
+        private const val PREFERENCE_CONFIGURATION_NAME = "configuration"
+        private const val FIRST_TIME = "isFirstRun"
+    }
+}
