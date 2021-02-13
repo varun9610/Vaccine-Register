dc27c3111d860d547b9095f96e08e8dcea977625
Varun Mehta <varun.mehta@hey.com>
2021-02-14 02:23:45 +0530

    Change dataBinding to viewBinding in MainActivity.kt
    Signed-off-by: Varun Mehta <varun.mehta@hey.com>

--- app/src/main/java/com/android/vaccineregister/MainActivity.kt
+++ app/src/main/java/com/android/vaccineregister/MainActivity.kt
@@ -2,14 +2,15 @@ package com.android.vaccineregister
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import com.android.vaccineregister.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding =
-            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
