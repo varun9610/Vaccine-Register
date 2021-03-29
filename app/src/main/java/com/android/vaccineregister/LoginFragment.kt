8caf98d5c1304434c6a6835745098fa9dc65f496
Varun Mehta <varun.mehta@hey.com>
2021-03-29 14:00:40 +0530

    Finish RegistrationFragment.kt and LoginFragment.kt flow
    Signed-off-by: Varun Mehta <varun.mehta@hey.com>

--- app/src/main/java/com/android/vaccineregister/LoginFragment.kt
+++ app/src/main/java/com/android/vaccineregister/LoginFragment.kt
@@ -1,16 +1,24 @@
 package com.android.vaccineregister
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.text.TextUtils
+import android.view.*
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.android.vaccineregister.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
+    private lateinit var auth: FirebaseAuth
+
+    private var progressBar: ProgressBar? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -21,10 +29,127 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.loginbtn.setOnClickListener {
+            onClick()
+        }
+        setProgressBar(binding.progressBar2)
+        auth = Firebase.auth
+
         binding.forgotpasswordtv.setOnClickListener { v: View ->
             v.findNavController().navigate(R.id.action_loginFragment_to_forgetPasswordFragment)
         }
+        checkCurrentUser()
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    private fun onClick() {
+        signIn(binding.etEmailID.text.toString(), binding.etLoginPassword.text.toString())
+    }
+
+    private fun signIn(email: String, password: String) {
+
+        if (!validateForm()) {
+            return
+        }
+        showProgressBar()
+
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    view?.findNavController()
+                        ?.navigate(R.id.action_loginFragment_to_postLoginHomepageFragment)
+                } else {
+                    // If sign in fails, display a message to the user.
+
+                    Toast.makeText(
+                        requireContext(), "Email/Password entered is wrong.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+
+                }
+
+                // [START_EXCLUDE]
+                if (!task.isSuccessful) {
+                    Toast.makeText(
+                        requireContext(), "Failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                // [END_EXCLUDE]
+                hideProgressBar()
+            }
+        // [END sign_in_with_email]
+    }
+
+    private fun setProgressBar(bar: ProgressBar) {
+        progressBar = bar
+    }
+
+    private fun showProgressBar() {
+        progressBar?.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        progressBar?.visibility = View.INVISIBLE
+    }
+
+    private fun validateForm(): Boolean {
+        val email = binding.etEmailID.text.toString()
+        var valid = true
+
+        if (TextUtils.isEmpty(email)) {
+            binding.etEmailID.error = "Required."
+            valid = false
+        } else {
+            binding.etEmailID.error = null
+        }
+
+        val password = binding.etLoginPassword.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            binding.etLoginPassword.error = "Required."
+            valid = false
+        } else {
+            binding.etLoginPassword.error = null
+        }
+
+        return valid
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkCurrentUser()
+    }
+
+    private fun checkCurrentUser() {
+        // [START check_current_user]
+        val user = Firebase.auth.currentUser
+        if (user != null) {
+            // User is signed in
+            view?.findNavController()
+                ?.navigate(R.id.action_loginFragment_to_postLoginHomepageFragment)
+        } else {
+            // No user is signed in
+            return
+        }
+        // [END check_current_user]
+    }
+
+    // Method is called when options menu is created
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.before_login, menu)
+    }
+
+    // Method is called when an option is selected from options menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/android/vaccineregister/RegistrationFragment.kt
+++ app/src/main/java/com/android/vaccineregister/RegistrationFragment.kt
@@ -1,22 +1,34 @@
 package com.android.vaccineregister
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.android.vaccineregister.databinding.FragmentRegistrationBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class RegistrationFragment : Fragment() {
 
     private var _binding: FragmentRegistrationBinding? = null
+    private var progressBar: ProgressBar? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
 
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
+    val db = Firebase.firestore
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +36,13 @@ class RegistrationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentRegistrationBinding.inflate(inflater, container, false)
+        binding.button.setOnClickListener {
+            onClick()
+        }
+        setProgressBar(binding.progressBar)
+
+        auth = Firebase.auth
+
         binding.logintv.setOnClickListener {
             it.findNavController().navigate(R.id.action_registrationFragment_to_loginFragment)
         }
@@ -32,6 +51,126 @@ class RegistrationFragment : Fragment() {
                 .navigate(R.id.action_registrationFragment_to_forgetPasswordFragment)
         }
         return binding.root
+    }
+
+    fun setProgressBar(bar: ProgressBar) {
+        progressBar = bar
+    }
+
+    fun showProgressBar() {
+        progressBar?.visibility = View.VISIBLE
+    }
+
+    fun hideProgressBar() {
+        progressBar?.visibility = View.INVISIBLE
+    }
+
+
+    private fun onClick() {
+        createAccount(
+            binding.editTextTextEmailAddress3.text.toString(),
+            binding.etpassword.text.toString()
+        )
+    }
+
+    private fun createAccount(email: String, password: String) {
+        if (!validateForm()) {
+            return
+        }
+        showProgressBar()
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(
+                        requireContext(), "Signup Successful",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    val user = hashMapOf(
+                        "uuid" to auth.uid,
+                        "email" to binding.editTextTextEmailAddress3.text.toString(),
+                        "name" to binding.etFullName.text.toString(),
+                        "phonenumber" to binding.editTextTextEmailAddress4.text.toString()
+                    )
+                    db.collection("users")
+                        .add(user)
+                    sendEmailVerification()
+                    binding.editTextTextEmailAddress3.setText("")
+                    binding.etFullName.setText("")
+                    binding.editTextTextEmailAddress4.setText("")
+                    binding.etpassword.setText("")
+
+                } else {
+                    // If sign in fails, display a message to the user.
+
+                    Toast.makeText(
+                        requireContext(),
+                        "Registration failed. Please try again after some time.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+                hideProgressBar()
+            }
+        // [END create_user_with_email]
+    }
+
+
+    private fun sendEmailVerification() {
+
+
+        // Send verification email
+        // [START send_email_verification]
+        val user = auth.currentUser!!
+        user.sendEmailVerification()
+            .addOnCompleteListener(requireActivity()) { task ->
+                // [START_EXCLUDE]
+
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Verification email sent to ${user.email} ",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Log.e(TAG, "sendEmailVerification", task.exception)
+                    Toast.makeText(
+                        requireContext(),
+                        "Failed to send verification email.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                // [END_EXCLUDE]
+            }
+        // [END send_email_verification]
+    }
+
+    private fun validateForm(): Boolean {
+        val email = binding.editTextTextEmailAddress3.text.toString()
+        var valid = true
+
+        if (TextUtils.isEmpty(email)) {
+            binding.editTextTextEmailAddress3.error = "Required."
+            valid = false
+        } else {
+            binding.editTextTextEmailAddress3.error = null
+        }
+
+        val password = binding.etpassword.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            binding.etpassword.error = "Required."
+            valid = false
+        } else {
+            binding.etpassword.error = null
+        }
+
+        return valid
+    }
+
+    companion object {
+        private const val TAG = "Main"
     }
 
     override fun onDestroyView() {
